7d339ffcf55600d5c450bbce8c7989aadadc0018
Daniel Day <daniel.sindt.day@gmail.com>
2015-10-24 16:12:42 -0400

    Updated bowtie_align script to split the reads into chunks (current 1 million reads or 4 million lines) within the tmp folder to better utlize the cluster by splitting the alignment across more nodes. Although, the non-parallel version of the script still breaks it into chunks, so this should get a further update someday to not do this splitting when the parallel option is not on.

--- bin/bowtie_align.sh
+++ bin/bowtie_align.sh
@@ -5,14 +5,30 @@ source ~/dsday/origami/bin/dispatch.sh
 OUTDIR=$1
 PARALLEL=$2
 
-dispatch "bowtie -n 1 -m 1 -p 8 --sam /nfs/genomes/human_gp_feb_09/bowtie/hg19 $OUTDIR/tmp/left_kept.fq > $OUTDIR/tmp/left_kept.sam"
-dispatch "bowtie -n 1 -m 1 -p 8 --sam /nfs/genomes/human_gp_feb_09/bowtie/hg19 $OUTDIR/tmp/right_kept.fq > $OUTDIR/tmp/right_kept.sam"
+# Split into 1 million reads per file (1 FASTQ read = 4 lines)
+dispatch "split -l 4000000 $OUTDIR/tmp/left_kept.fq $OUTDIR/tmp/leftkept"
+dispatch "split -l 4000000 $OUTDIR/tmp/right_kept.fq $OUTDIR/tmp/rightkept"
 
 wait
 
-dispatch "samtools view -Sb $OUTDIR/tmp/left_kept.sam > $OUTDIR/tmp/left_kept.bam"
-dispatch "samtools view -Sb $OUTDIR/tmp/right_kept.sam > $OUTDIR/tmp/right_kept.bam"
+for FILE in $OUTDIR/tmp/leftkept*
+do
+	dispatch "bowtie -n 1 -m 1 -p 6 --sam /nfs/genomes/human_gp_feb_09/bowtie/hg19 $FILE > $FILE.sam; samtools view -Sb $FILE.sam > $FILE.bam; rm $FILE.sam"
+done
 
+for FILE in $OUTDIR/tmp/rightkept*
+do
+	dispatch "bowtie -n 1 -m 1 -p 6 --sam /nfs/genomes/human_gp_feb_09/bowtie/hg19 $FILE > $FILE.sam; samtools view -Sb $FILE.sam > $FILE.bam; rm $FILE.sam"
+done
+
+wait
+
+dispatch "samtools merge $OUTDIR/tmp/left_kept.bam $OUTDIR/tmp/leftkept*.bam"
+dispatch "samtools merge $OUTDIR/tmp/right_kept.bam $OUTDIR/tmp/rightkept*.bam"
+
+wait
+
+dispatch "rm $OUTDIR/tmp/leftkept* $OUTDIR/tmp/rightkept*"
 wait
 
 dispatch "samtools sort -Obam -Tlefttmp -n $OUTDIR/tmp/left_kept.bam > $OUTDIR/tmp/left_kept.sorted.bam"
