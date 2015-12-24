9b7c8579b16064004052fa42d5622b39655d4959
Daniel Day <daniel.sindt.day@gmail.com>
2015-12-23 20:42:18 -0500

    Model file added

---
+++ run-model.r
@@ -0,0 +1,58 @@
+convert.to.factor <- function(pos,idx,l=NULL) {
+  d <- pos[,idx]
+  s <- paste(d[,1],d[,2],d[,3],sep='_')
+  
+  r <- if(is.null(l)) factor(s) else factor(s,levels=levels(l)) 
+  r
+}
+
+
+depth <- read.table("peak-counts.txt",sep='\t')
+intcounts <- read.table("int-counts.txt",sep='\t')
+
+f <- convert.to.factor(depth,1:3)
+
+i1 <- convert.to.factor(intcounts,1:3,f)
+i2 <- convert.to.factor(intcounts,4:6,f)
+
+p <- intcounts[i1 != i2,]
+
+params <- lapply(1:nrow(p),function(i) list(I=c(0),w=c(0)))
+
+N <- 1000
+
+f <- function(x,i,w) {
+  if(i==1 ) w * dpois(x,lambda=5) else (1-w) * dpois(x,lambda=1)
+}
+
+runupdate <- function(v,l) {
+  n <- length(l[[1]])
+  
+  p <- l$w[[n]]*dpois(v,lambda=5)/sum(l$w[[n]]*dpois(v,lambda=5)+(1-l$w[[1]])*dpois(v,lambda=1))
+  print(p)
+  i <- rbinom(1,1,p)
+  w <- runif(1,0,1)
+  
+  l$I[[n+1]] <- i
+  l$w[[n+1]] <- w
+  
+  l
+}
+
+#for( i in 1:N) {
+#  for( j in 1:nrow(p)) {
+#    l <- params[[j]]
+#    
+#    r <- length(l[[1]])
+#    
+#    i <- l$i[r]
+#    w <- l$w[r]
+#    
+#    i.p <- rbinom(1,1,w)
+#    
+#    r <- f(p[j,7],i.p,w)
+#    
+#    params[[j]] <- l
+#  }
+#}
+
