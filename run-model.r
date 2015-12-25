bd1c03b4e51b06ed2488f271a116b80a948aa688
Daniel Day <daniel.sindt.day@gmail.com>
2015-12-24 23:59:21 -0500

    Added lambda estimation

--- run-model.r
+++ run-model.r
@@ -17,7 +17,7 @@ i2 <- convert.to.factor(intcounts,4:6,f)
 
 p <- intcounts[i1 != i2,]
 
-params <- lapply(1:nrow(p),function(i) list(I=c(0),w=c(0)))
+params <- lapply(1:nrow(p),function(i) list(I=c(0),w=c(.5),lambda1=2))
 
 N <- 1000
 
@@ -28,31 +28,23 @@ f <- function(x,i,w) {
 runupdate <- function(v,l) {
   n <- length(l[[1]])
   
-  p <- l$w[[n]]*dpois(v,lambda=5)/sum(l$w[[n]]*dpois(v,lambda=5)+(1-l$w[[1]])*dpois(v,lambda=1))
-  print(p)
+  p <- l$w[[n]]*dpois(v,lambda=l$lambda1[[n]])/sum(l$w[[n]]*dpois(v,lambda=l$lambda1[[n]])+(1-l$w[[1]])*dpois(v,lambda=1))
+  #print(p)
   i <- rbinom(1,1,p)
-  w <- runif(1,0,1)
+  w <- rbeta(1,1+i,1+(1-i))
+  lambda1 <- 1
+  while(lambda1 <= 1) lambda1 <- rnorm(1,weighted.mean(c(1,l$lambda1[[n]]),c(1-w,w)),1)
   
   l$I[[n+1]] <- i
   l$w[[n+1]] <- w
+  l$lambda1[[n+1]] <- lambda1
   
   l
 }
 
-#for( i in 1:N) {
-#  for( j in 1:nrow(p)) {
-#    l <- params[[j]]
-#    
-#    r <- length(l[[1]])
-#    
-#    i <- l$i[r]
-#    w <- l$w[r]
-#    
-#    i.p <- rbinom(1,1,w)
-#    
-#    r <- f(p[j,7],i.p,w)
-#    
-#    params[[j]] <- l
-#  }
-#}
-
+for( i in 1:N) {
+  print(i)
+  for( j in 1:50) {
+    params[[j]] <- runupdate(p[j,7],params[[j]])
+  }
+}
