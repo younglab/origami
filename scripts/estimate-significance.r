f097e36bac98510a41af909573219068ff603626
Daniel Day <daniel.sindt.day@gmail.com>
2015-12-26 02:59:53 -0500

    Moved files around and set basis for updated processing of interactions

---
+++ scripts/estimate-significance.r
@@ -0,0 +1,18 @@
+convert.to.factor <- function(pos,idx,l=NULL) {
+  d <- pos[,idx]
+  s <- paste(d[,1],d[,2],d[,3],sep='_')
+  
+  r <- if(is.null(l)) factor(s) else factor(s,levels=levels(l)) 
+  r
+}
+
+
+depth <- read.table("peak-counts.txt",sep='\t')
+intcounts <- read.table("int-counts.txt",sep='\t')
+
+f <- convert.to.factor(depth,1:3)
+
+i1 <- convert.to.factor(intcounts,1:3,f)
+i2 <- convert.to.factor(intcounts,4:6,f)
+
+p <- intcounts[i1 != i2,]
