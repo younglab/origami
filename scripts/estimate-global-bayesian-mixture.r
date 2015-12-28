a851d8401cc8765a5c1855d15d7be191159fd598
Daniel Day <daniel.sindt.day@gmail.com>
2015-12-27 19:55:47 -0500

    Updated prior distribituions for estimating lambda parameters that are more appropriate for a Poisson model. Works better but still a few bugs when running it on all the data.

--- scripts/estimate-global-bayesian-mixture.r
+++ scripts/estimate-global-bayesian-mixture.r
@@ -10,17 +10,19 @@ estimate.global.bayesian.mixture <- function(ints,depth,N=1000) {
   lambda1 <- c(5)
   hsd0 <- hsd1 <- sd(counts)
   p1 <- c(.5)
-  a <- c(1)
-  b <- c(1)
+  a0 <- c(1)
+  b0 <- c(1)
+  a1 <- c(1)
+  b1 <- c(1)
   
   f <- function(p,h1,h0) p*dpois(counts,h1)/rowSums(cbind(p*dpois(counts,h1),(1-p)*dpois(counts,h0)))
   
   for( i in 1:N ) {
 #    print(c(lambda1[i],lambda0[i]))
     vp <- f(sapply(l,function(v) v$p1[i]),lambda1[i],lambda0[i])
-#    print(vp)
+    print(vp)
     vz <- rbinom(S,1,vp)
-    print(sum(vz))
+    #print(sum(vz))
     l <- mapply(function(lx,z){
       p <- rbeta(1,1+z,1+(1-z))
       lx$z[i] <- z
@@ -29,31 +31,33 @@ estimate.global.bayesian.mixture <- function(ints,depth,N=1000) {
     },l,as.list(vz),SIMPLIFY=F)
     
     l0 <- 0
-    r <- max(mean(counts[vz==0]),0,na.rm=T) ## default to 0
+    r <- sum(counts[vz==0])
     #print(r)
    # print((a[i]*lambda0[i]+sum(vz==0)*r)/(a[i]+sum(vz)))
-    while(l0 <= 0 ) l0 <- rnorm(1,(a[i]*lambda0[i]+sum(vz==0)*r)/(a[i]+sum(vz==0)),hsd0)
-    a[i+1] <- a[i]+sum(vz==0)+1
-    if(sum(vz==0) >= 2) hsd0 <- sd(c(counts[vz==0]))
+    while(l0 <= 0 ) l0 <- rgamma(1,a0[i]+r,b0[i]+sum(vz==0))
+    a0[i+1] <- a0[i]+r
+    b0[i+1] <- b0[i]+sum(vz==0)
 #    print(l0)
     l1 <- l0
-    r <- max(mean(counts[vz==1]),0,na.rm=T) ## default to 0
+    r <- sum(counts[vz==1])
+    #print(r)
     
+    #print(c(a1[i]+r,b1[i]+sum(vz==1)))
     while(l1 <= l0 ) {
-      l1 <- rnorm(1,(b[i]*lambda1[i]+sum(vz==1)*r)/(b[i]+sum(vz==1)),hsd1)
-      print(c(l0,l1))
+      l1 <- rgamma(1,a1[i]+r,b1[i]+sum(vz==1))
+
+#      print(c(l0,l1))
     }
-    b[i+1] <- b[i] + sum(vz==1)+1
-    if( sum(vz==1) >= 2)hsd1 <- sd(c(counts[vz==1]))
-   print(c(hsd0,hsd1))
+    a1[i+1] <- a1[i]+r
+    b1[i+1] <- b1[i]+sum(vz==1)
     
     
     lambda0[i+1] <- l0
     lambda1[i+1] <- l1
-    print('---')
+    #print('---')
   }
   
-  list(s=l,l0=lambda0,l1=lambda1,a=a,b=b)
+  list(s=l,l0=lambda0,l1=lambda1,a0=a0,b0=b0,a1=a1,b1=b1)
 }
 
 extract.global.bayesian.prob <- function(model) {
