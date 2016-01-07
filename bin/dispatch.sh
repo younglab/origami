f52f554dae59ccb4fc027d70a835745bf1d94d99
Daniel Day <daniel.sindt.day@gmail.com>
2016-01-06 23:27:10 -0500

    Fixed usage of bash variable in dispatch code

--- bin/dispatch.sh
+++ bin/dispatch.sh
@@ -3,7 +3,7 @@
 dispatch() {
         if [ "$PARALLEL" = on ]
         then
-                bsub -K -q normal -J origami -o $OUTDIR/logs/cluster_log.txt "$@" &
+                bsub -K -q normal -J origami -o $OUTPUTDIR/logs/cluster_log.txt "$@" &
         else
                 eval "$@"
         fi
