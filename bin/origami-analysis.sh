9380f41ede93b15090a7149d753e2d5551455c83
Daniel Day <daniel.sindt.day@gmail.com>
2016-01-12 14:03:08 -0500

    Updated analysis pipeline frontend

--- bin/origami-analysis.sh
+++ bin/origami-analysis.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "Finding interactions"
-cut -f 1,2,3,4,5,6 peaks_peaks.narrowPeak | bedtools pairtobed -bedpe -abam mapped_reads.bam -b - > raw-interactions.bedpe
+#cut -f 1,2,3,4,5,6 peaks_peaks.narrowPeak | bedtools pairtobed -bedpe -abam mapped_reads.bam -b - > raw-interactions.bedpe
+
+Rscript ~/dsday/origami/scripts/estimate-interaction-counts.r 
+Rscript ~/dsday/origami/scripts/estimate-significance.r 
