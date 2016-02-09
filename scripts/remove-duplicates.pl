4c05467d78e695446c97135a48660cb89872d693
Daniel Day <daniel.sindt.day@gmail.com>
2016-02-09 13:02:47 -0500

    Added new Perl script to help filter through PETs and identify duplicated events (need to optimize).

---
+++ scripts/remove-duplicates.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+
+open(F,"samtools view -h $ARGV[0]|") or die "Cannot open $ARGV[0]: $!";
+
+my %h;
+my $n = 0;
+my $ex = 0;
+my $line1;
+my $line2;
+
+while(<F>) {
+  $line1 = $_;
+  print;
+  last unless /^@/;
+}
+
+chomp $line1;
+$line2 = <F>;
+chomp $line2;
+while(!eof(F)) {
+  my @a1 = split /\t/, $line1;
+  my @a2 = split /\t/, $line2;
+  $n++;
+  # concatenate the reference name and position as a key
+  my $s = "$a1[2]:$a1[3]:$a2[2]:$a2[3]";
+  if(!exists($h{$s})) {
+    $h{$s} = 1;
+    print "$line1\n$line2\n";
+  } else {
+    $ex++;
+    print STDERR "Excluded $a1[0]:$a2[0]\n";
+  }
+  $line1 = <F>;
+  $line2 = <F>;
+}
+
+print STDERR "Processed $n PETs, excluded $ex PETs because of duplication\n";
+
+close(F);
