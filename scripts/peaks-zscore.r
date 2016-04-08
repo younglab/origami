9a7170dcc42600aaf93f3ecc074fdd9c4a0828df
Daniel Day <daniel.sindt.day@gmail.com>
2016-04-08 12:35:18 -0400

    Added check for whether any peaks had no PET counts to filter them out (happens typically when the peak calls are not from the ChIA-PET data itself)

--- scripts/peaks-zscore.r
+++ scripts/peaks-zscore.r
@@ -8,6 +8,12 @@ peaks <- read.table(peakfile,sep='\t')
 
 counts <- log10(peaks$V4)
 
+if(any(is.infinite(counts))) {
+  b <- is.infinite(counts)
+  counts <- counts[!b]
+  warning("\tSome peaks were removed with having 0 PET counts, this generally happens when using a set of peak calls not derived from the ChIA-PET data")
+}
+
 zscore <- (counts - mean(counts))/sd(counts)
 
 m <- peaks[zscore<threshold,1:3]
