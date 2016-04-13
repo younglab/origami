4198ae6c25b2adaf160b77f3180a84a463dad28d
Daniel Day <daniel.sindt.day@gmail.com>
2016-04-13 17:47:37 -0400

    Fixed typo with variable name

--- scripts/estimate-global-bayesian-mixture.r
+++ scripts/estimate-global-bayesian-mixture.r
@@ -19,7 +19,7 @@ estimate.global.bayesian.mixture <- function(ints,depth,inttable,N=1100,burnin=1
   counts <- ints[,7]
   d <- depth[,4]
   intdist <- distance(g1,g2)
-  interchromsomal <- is.na(intdist)
+  interchromosomal <- is.na(intdist)
   minintdist <- min(intdist[!interchromosomal])
   #if(any(is.na(intdist))) {
   #  intdist[is.na(intdist)] <- max(intdist,na.rm=T)
@@ -143,22 +143,18 @@ estimate.global.bayesian.mixture <- function(ints,depth,inttable,N=1100,burnin=1
     ret$lambda1[i+1] <- l1
     
     if(with.distance.weight) {
-      x <- log10(intdist[vz==1 & !interchromsomal]+1)
+      x <- log10(intdist[vz==1 & !interchromosomal]+1)
       
       s1 <- if( usedf > 0 ) smooth.spline(x,pmax(counts[vz==1& !is.na(intdist)]-l1,0),df=usedf) else smooth.spline(x,pmax(counts[vz==1& !is.na(intdist)]-l1,0))
-      x <- log10(intdist[vz==0 & !interchromsomal]+1)
+      x <- log10(intdist[vz==0 & !interchromosomal]+1)
       
       s0 <- if( usedf > 0 ) smooth.spline(x,pmax(counts[vz==0& !is.na(intdist)]-l1,0),df=usedf) else smooth.spline(x,pmax(counts[vz==0& !is.na(intdist)]-l0,0))
       
-      x <- log10(intdist)
-      if(any(interchromsomal)) x[interchromsomal] <- minintdist ## set eact interchromsomal interaction to shortest distance (which should have the highest mean read count)
+      x <- log10(intdist+1)
+      if(any(interchromosomal)) x[interchromosomal] <- minintdist ## set eact interchromsomal interaction to shortest distance (which should have the highest mean read count)
       
       lambdad1 <- predict(s1,x)$y
-      #if(any(interchromsomal)) lambdad1[is.na(x)] <- 0 ## is there better way to handle this?
-      
-      
       lambdad0 <- predict(s0,x)$y
-      #if(any(interchromsomal)) lambdad0[is.na(x)] <- 0
       
       if( !mini.model) {
         ret$lambdad1[[i]] <- lambdad1
