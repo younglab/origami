bc947b14c887c4cc02aa865e9c69b9454100713e
Daniel Day <daniel.sindt.day@gmail.com>
2016-06-22 11:47:30 -0400

    Updated simulation code such that it now can generate a full model with sampling of 'true' interactions based on some weight factor

--- simulation/run-chiapet-simulation.r
+++ simulation/run-chiapet-simulation.r
@@ -1,13 +1,15 @@
 library(GenomicRanges)
 
 read.in.binding.sites <- function(fname) {
-  temp <- read.table(fname,sep='\t')
+  temp <- read.table(fname)
   
   GRanges(seqnames=temp$V1,ranges=IRanges(temp$V2,temp$V3),strand='*')
 }
 
 read.in.interactions <- function(fname) {
-  temp <- read.table(fname,sep='\t')
+  temp <- read.table(fname)
+  
+  temp[,3] <- temp[,3]/sum(temp[,3])
   
   temp
 }
@@ -15,11 +17,14 @@ read.in.interactions <- function(fname) {
 simulate.pets <- function() {
   p <- runif(1,0,1)
   
-  if(p>.8) {
+  if(p<prandom) { ## random ligation
     i <- sort(sample.int(length(binding),size=2,replace=T))
     r <- c(i[1],i[2])
+  } else if(prandom < p && p < (prandom+pself)) {
+    idx <- sample.int(length(binding),size=1)
+    r <- c(idx,idx)
   } else {
-    idx <- sample.int(nrow(ints),size=1)
+    idx <- sample.int(nrow(ints),size=1,prob=ints[,3])
     r <- c(ints[idx,1],ints[idx,2])
   }
   
@@ -30,9 +35,21 @@ simulate.pets <- function() {
 
 if( !interactive() ) args <- commandArgs(T)
 
+if(length(args)<5) {
+  stop("args: binding file, int file, N PETs, P(self), P(random)")
+}
+
 binding <- read.in.binding.sites(args[1])
 ints <- read.in.interactions(args[2])
 npets <- as.integer(args[3])
+pself <- as.numeric(args[4])
+prandom <- as.numeric(args[5])
+pint <- 1-pself-prandom
+print(pint)
+
+#if(sum(c(pself,prandom,pint)!=1)) {
+#  stop("probabilities need to add up to 1")
+#}
 
 m <- do.call(rbind,replicate(npets,simulate.pets(),simplify = F))
 
