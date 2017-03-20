47aa5038ab6718183196246adcb23bc35f17d197
Daniel Day <daniel.sindt.day@gmail.com>
2017-03-20 13:32:24 -0400

    Added procedure to downsample PETs from a BAM file

---
+++ scripts/downsample-bam-file.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use strict;
+
+die "Syntax: <BAM file> <number of pairs> <tmp file> <output file> <sorted output file>" unless scalar(@ARGV)>=5;
+
+my ($bamfile,$npairs,$tmpfile,$outputfile,$sortedoutputfile) = @ARGV;
+
+
+die "Cannot find $bamfile" unless -e $bamfile;
+die "Number of pairs has to be non-negative" unless $npairs > 0;
+
+my %pairs;
+
+open(B,"samtools view -h $bamfile") or die "Cannot read BAM file $bamfile: $!";
+open(O,">","$tmpfile") or die "Cannot write to $tmpfile: $!";
+
+while(<B>) {
+  if( /^@/ ) {
+    print O;
+    next;
+  }
+  
+  my $p1 = $_;
+  my $p2 = <B>;
+  
+  my ($r1) = split /\t/, $p1;
+  my ($r2) = split /\t/, $p2;
+  
+  die "BAM file does not look paired, saw $r1 and $r2" unless $r1 eq $r2;
+  
+  $pairs{$r1} = [$p1,$p2];
+}
+
+close(B);
+
+die "No paired-end reads found in BAM file!" if scalar(keys(%pairs))==0;
+
+for( my $i = 0; $i < $npairs; $i++ ) {
+  my $random_key = keys(%pairs)[rand keys(%pairs)];
+  my @pets = @{$pairs{$random_key}};
+  
+  print O for(@pets);
+}
+
+close(O);
+
+my $output = `samtools view -Sb $tmpfile > $outputfile`;
+
+die "Failed to covert $tmpfile into $outputfile: $output" unless $? == 0;
+
+unlink($tmpfile);
+
+my $output = `samtools sort -@8 -Ttmp $outputfile > $sortedoutputfile`;
+
+die "Failed to sort BAM file: $output" unless $? == 0;
+
